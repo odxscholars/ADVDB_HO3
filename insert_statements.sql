18a3d86598ff18b6bcb13b65c80630c98fde7b18
odx <shemgithub@protonmail.com>
2024-03-05 10:24:56 +0800

    added the appropriate directories

---
+++ insert_statements.sql
@@ -0,0 +1,10 @@
+
+DELETE FROM Accounts;
+ALTER TABLE Accounts AUTO_INCREMENT = 1;
+
+INSERT INTO Accounts (amount) VALUES (1000);
+INSERT INTO Accounts (amount) VALUES (2000);
+INSERT INTO Accounts (amount) VALUES (3000);
+INSERT INTO Accounts (amount) VALUES (4000);
+INSERT INTO Accounts (amount) VALUES (5000);
+
--- schema_create.sql
+++ schema_create.sql
@@ -1,16 +1,8 @@
-CREATE TABLE Accounts (
+CREATE TABLE IF NOT EXISTS Accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     amount INT
 ) ENGINE=InnoDB;
 
 
-DELETE FROM Accounts;
 
-ALTER TABLE Accounts AUTO_INCREMENT = 1;
-
-INSERT INTO Accounts (amount) VALUES (1000);
-INSERT INTO Accounts (amount) VALUES (2000);
-INSERT INTO Accounts (amount) VALUES (3000);
-INSERT INTO Accounts (amount) VALUES (4000);
-INSERT INTO Accounts (amount) VALUES (5000);
 
