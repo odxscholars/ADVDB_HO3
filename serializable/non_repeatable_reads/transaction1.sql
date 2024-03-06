9e7aa2a386229a9d3091e135fa9b448ecd80c5a5
Mark Daniel Gutierrez <markdcg@outlook.com>
2024-03-06 19:35:08 +0800

    refactored

--- serializable/non_repeatable_reads/transaction1.sql
+++ serializable/non_repeatable_reads/transaction1.sql
@@ -2,7 +2,9 @@ SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
 START TRANSACTION;
 
-SELECT amount FROM Accounts WHERE id = 1;
-DO SLEEP(5);
+SELECT amount FROM Accounts where id = 1; # amount = 1000
 
-COMMIT;
+DO SLEEP(15);
+# After doing transaction2.sql
+SELECT amount FROM Accounts where id = 1; # amount = 1001
+ COMMIT;
--- serializable/phantom_reads/transaction1.sql
+++ serializable/phantom_reads/transaction1.sql
@@ -2,8 +2,10 @@ SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
 START TRANSACTION ;
 
-SELECT COUNT(*) FROM Accounts WHERE amount > 150;
-DO SLEEP(5);
+SELECT id FROM accounts where amount > 1000;
+# Should result in id: 2,3,4,5
+
+SELECT id FROM accounts where amount > 1000;
 
 COMMIT;
 
