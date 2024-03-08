d4570c358ce7dd983bc0f990451e000ff2c08bd9
Mezen Lababidi <mez_lababidi@dlsu.edu.ph>
2024-03-08 14:38:43 +0800

    repeatable_read + capitalization

--- serializable/phantom_reads/transaction1.sql
+++ serializable/phantom_reads/transaction1.sql
@@ -2,10 +2,10 @@ SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
 START TRANSACTION ;
 
-SELECT id FROM accounts where amount > 1000;
+SELECT id FROM Accounts where amount > 1000;
 # Should result in id: 2,3,4,5
 
-SELECT id FROM accounts where amount > 1000;
+SELECT id FROM Accounts where amount > 1000;
 
 COMMIT;
 
